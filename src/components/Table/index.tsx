83b1ca7a40dc601d143c8428575ae093aefd0a7e
leminhtuan <leminhtuanlmt2k1@gmail.com>
2023-05-12 16:31:12 +0700

    device page

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './table.css';
 import { useMemo } from 'react';
 import { useTable } from 'react-table';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Table({ columns, data }: any) {
   const memoizedColumns = useMemo(() => columns, [columns]);
@@ -10,6 +12,42 @@ function Table({ columns, data }: any) {
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
+  const renderActiveStatus = (cellValue: any) => {
+    if (cellValue === 'Hoạt động') {
+      return (
+        <div className="status__space">
+          <FontAwesomeIcon icon={faCircle} color="#34CD26" />
+          <span className="text__status">Hoạt động</span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="status__space">
+          <FontAwesomeIcon icon={faCircle} color="red" />
+          <span className="text__status">Ngưng hoạt động</span>
+        </div>
+      );
+    }
+  };
+
+  const renderConnectStatus = (cellValue: any) => {
+    if (cellValue === 'Kết nối') {
+      return (
+        <div className="status__space">
+          <FontAwesomeIcon icon={faCircle} color="#34CD26" />
+          <span className="text__status">Kết nối</span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="status__space">
+          <FontAwesomeIcon icon={faCircle} color="red" />
+          <span className="text__status">Mất kết nối</span>
+        </div>
+      );
+    }
+  };
+
   return (
     <table className="table" {...getTableProps()}>
       <thead>
@@ -18,6 +56,8 @@ function Table({ columns, data }: any) {
             {headerGroup.headers.map(column => (
               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
+            <th></th>
+            <th></th>
           </tr>
         ))}
       </thead>
@@ -25,10 +65,19 @@ function Table({ columns, data }: any) {
         {rows.map(row => {
           prepareRow(row);
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              ))}
+            <tr className="tr__body" {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                if (cell.column.id === 'activeStatus') {
+                  return <td {...cell.getCellProps()}>{renderActiveStatus(cell.value)}</td>;
+                } else if (cell.column.id === 'conectStatus') {
+                  return <td {...cell.getCellProps()}>{renderConnectStatus(cell.value)}</td>;
+                } else {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                }
+              })}
+
+              <td>Chi tiết</td>
+              <td>Cập nhật</td>
             </tr>
           );
         })}
